8f5362ab3a05f07a6ecf27441fd4848acb09eb6e
Peter Sauer <peter.sauer@sciencesoft.at>
2025-01-14 12:04:28 +0100

    Flutter version upgrade.

--- lib/flutter_internals.dart
+++ lib/flutter_internals.dart
@@ -1,14 +1,14 @@
 const Map<String,String> flutterInternals = 
 {
-  'frameworkVersion': '3.27.1',
+  'frameworkVersion': '3.27.2',
   'channel': 'stable',
   'repositoryUrl': 'https://github.com/flutter/flutter.git',
-  'frameworkRevision': '17025dd88227cd9532c33fa78f5250d548d87e9a',
-  'frameworkCommitDate': '2024-12-17 03:23:09 +0900',
-  'engineRevision': 'cb4b5fff73850b2e42bd4de7cb9a4310a78ac40d',
-  'dartSdkVersion': '3.6.0',
+  'frameworkRevision': '68415ad1d920f6fe5ec284f5c2febf7c4dd5b0b3',
+  'frameworkCommitDate': '2025-01-13 10:22:03 -0800',
+  'engineRevision': 'e672b006cb34c921db85b8e2f482ed3144a4574b',
+  'dartSdkVersion': '3.6.1',
   'devToolsVersion': '2.40.2',
-  'flutterVersion': '3.27.1',
+  'flutterVersion': '3.27.2',
   'flutterRoot': '/Users/pezi/fvm/versions/3.24.5'
 }
 ;
